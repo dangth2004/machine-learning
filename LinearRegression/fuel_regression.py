6e9bd4524efdf067865f4e92f9984034de74b362
dangth2004 <trinhhaidang2004@gmail.com>
2024-09-25 18:51:19 +0700

    Edit fuel_regression.py, add real_estate.csv

--- LinearRegression/fuel_regression.py
+++ LinearRegression/fuel_regression.py
@@ -1,6 +1,6 @@
 import math
 import numpy as np
-from sklearn import datasets, linear_model
+from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 # Read data
